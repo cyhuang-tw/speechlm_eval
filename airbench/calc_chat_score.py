f6463196f15037f4a0af0e2f0553cf8726d53812
Chien-yu Huang <cyhuang1997@gmail.com>
2026-01-24 23:38:50 -0600

    Updated AIR-Bench Chat score calculation script.

--- airbench/calc_chat_score.py
+++ airbench/calc_chat_score.py
@@ -4,68 +4,38 @@ from pathlib import Path
 
 
 def main(json_path: Path) -> None:
-    with json_path.open(mode="r") as fp:
+    with open(json_path, 'r') as fp:
         data = json.load(fp)
 
-    task_name_list = [
-        "speech",
-        "sound",
-        "music",
-        "speech_and_sound",
-        "speech_and_music",
-    ]
-    total_num_dict = {
-        "speech": 0,
-        "sound": 0,
-        "music": 0,
-        "speech_and_sound": 0,
-        "speech_and_music": 0,
-    }
-    win_num_dict = {
-        "speech": 0,
-        "sound": 0,
-        "music": 0,
-        "speech_and_sound": 0,
-        "speech_and_music": 0,
-    }
-    gpt4_score_sum_dict = {
-        "speech": 0,
-        "sound": 0,
-        "music": 0,
-        "speech_and_sound": 0,
-        "speech_and_music": 0,
-    }
-    llm_score_sum_dict = {
-        "speech": 0,
-        "sound": 0,
-        "music": 0,
-        "speech_and_sound": 0,
-        "speech_and_music": 0,
-    }
+    task_name_list = ['speech','sound','music','speech_and_sound','speech_and_music']
+    total_num_dict = {'speech':0, 'sound':0, 'music':0, 'speech_and_sound':0, 'speech_and_music':0}
+    win_num_dict = {'speech':0, 'sound':0, 'music':0, 'speech_and_sound':0, 'speech_and_music':0}
+    gpt4_score_sum_dict = {'speech':0, 'sound':0, 'music':0, 'speech_and_sound':0, 'speech_and_music':0}
+    llm_score_sum_dict = {'speech':0, 'sound':0, 'music':0, 'speech_and_sound':0, 'speech_and_music':0}
     fail_num = 0
     total_sum = 0
     for line in data:
-        task_name = line["task_name"]
-        if task_name == "speech_QA" or task_name == "speech_dialogue_QA":
-            task_id = "speech"
-        elif task_name == "music_QA" or task_name == "music_generation_analysis_QA":
-            task_id = "music"
-        elif task_name == "sound_QA" or task_name == "sound_generation_QA":
-            task_id = "sound"
-        elif task_name == "speech_and_sound_QA":
-            task_id = "speech_and_sound"
-        elif task_name == "speech_and_music_QA":
-            task_id = "speech_and_music"
+        task_name = line['task_name']
+        if task_name == 'speech_QA' or task_name == 'speech_dialogue_QA':
+            task_id = 'speech'
+        elif task_name == 'music_QA' or task_name == 'music_generation_analysis_QA':
+            task_id = 'music'
+        elif task_name == 'sound_QA' or task_name == 'sound_generation_QA':
+            task_id = 'sound'
+        elif task_name == 'speech_and_sound_QA':
+            task_id = 'speech_and_sound'
+        elif task_name == 'speech_and_music_QA':
+            task_id = 'speech_and_music'
         total_num = total_num_dict.get(task_id, 0)
         win_num = win_num_dict.get(task_id, 0)
         gpt4_socre_sum = gpt4_score_sum_dict.get(task_id, 0)
         llm_socre_sum = llm_score_sum_dict.get(task_id, 0)
-        gpt_score = line["gpt-score"].strip().replace("\n", "")
-        scores = gpt_score.split(" ")
+        gpt_score = line['gpt-score'].strip().replace('\n','')
+        scores = gpt_score.split(' ')
 
         if len(scores) == 2:
-            try:
-                # if True:
+            try: 
+            #if True:
                 total_num += 1
                 total_sum += 1
                 gpt_score = int(scores[0])
@@ -76,24 +46,21 @@ def main(json_path: Path) -> None:
                     exit(1)
                 gpt4_socre_sum += gpt_score
                 llm_socre_sum += llm_score
-                if "cir" in input_file:
-                    if gpt_score > llm_score:
-                        win_num += 1
-                else:
-                    if llm_score > gpt_score:
-                        win_num += 1
+                if llm_score > gpt_score:
+                    win_num += 1
 
             except:
-                print("gpt-4-turbo predict is: ", end="")
-                print(line["gpt-score"])
+                print("gpt-4-turbo predict is: ", end='')
+                print(line['gpt-score'])
                 fail_num += 1
                 continue
         else:
-            print("gpt-4-turbo predict is: ", end="")
-            print(line["gpt-score"])
+            print("gpt-4-turbo predict is: ", end='')
+            print(line['gpt-score'])
             fail_num += 1
             continue
 
+        
         total_num_dict[task_id] = total_num
         win_num_dict[task_id] = win_num
         gpt4_score_sum_dict[task_id] = gpt4_socre_sum
@@ -105,15 +72,17 @@ def main(json_path: Path) -> None:
         gpt4_avg_score = gpt4_score_sum_dict[task_id] / total_num
         llm_avg_score = llm_score_sum_dict[task_id] / total_num
         win_ratio = win_num / total_num
-        print(
-            f"{task_id}: Sum={total_num}, Win_Rate={win_ratio}, gpt4_avg_score={gpt4_avg_score}, llm_avg_score={llm_avg_score}"
-        )
+        print(f'{task_id}: Sum={total_num}, Win_Rate={win_ratio}, gpt4_avg_score={gpt4_avg_score}, llm_avg_score={llm_avg_score}')
 
     per = total_sum / (total_sum + fail_num)
-    print(f"fail_num: {fail_num}, success_num: {total_sum}, percentage: {per}")
-
+    print(f'fail_num: {fail_num}, success_num: {total_sum}, percentage: {per}')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--json_path", type=Path, required=True)
+    parser.add_argument(
+        "--json_path",
+        type=Path,
+        required=True,
+    )
     main(**vars(parser.parse_args()))
+
